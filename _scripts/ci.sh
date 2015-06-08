946fd803efb7116ae22ed16c1826eb06d7eeaa09
Viktor Benei <viktor.benei@gmail.com>
2015-06-08 16:17:35 +0200

    ci bash script (for Continuous Integration)

---
+++ _scripts/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${THIS_SCRIPT_DIR}/.."
+
+#
+# Script for Continuous Integration
+#
+
+set -v
+
+go get ./...
+
+bash _scripts/build.sh
