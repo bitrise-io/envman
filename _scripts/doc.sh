490413f7c9eb3c2146f218d3a8379a1a941f18ed
Viktor Benei <viktor.benei@gmail.com>
2015-06-16 13:31:14 +0200

    very basic documentation generator (by calling envman helps)

---
+++ _scripts/doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo "#############################"
+echo "envman --help"
+echo "#############################"
+echo
+envman --help
+
+echo
+echo "#############################"
+echo "envman add --help"
+echo "#############################"
+echo
+envman add --help
