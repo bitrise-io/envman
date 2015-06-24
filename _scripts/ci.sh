8963912e7393cc90428e86a3bce3f34600cb104e
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-06-24 13:57:05 +0200

    ci.sh fix

--- _scripts/ci.sh
+++ _scripts/ci.sh
@@ -9,15 +9,21 @@ cd "${THIS_SCRIPT_DIR}/.."
 # Script for Continuous Integration
 #
 
+# export PATH=$PATH:$GOPATH/bin
+# export GOPATH="${THIS_SCRIPT_DIR}/../Godeps/_workspace"
+# echo "GOPATH: $GOPATH"
+# mkdir $GOPATH/bin
+# export PATH=$PATH:$GOPATH/bin
+
 set -v
 
-export PATH=$PATH:$GOPATH/bin
-export GOPATH="${THIS_SCRIPT_DIR}/../Godeps/_workspace"
-export PATH=$PATH:$GOPATH/bin
+go get github.com/tools/godep
+godep restore
 
 bash _scripts/install.sh
 
 # Tests
+go get github.com/kisielk/errcheck
 go install github.com/kisielk/errcheck
 
 errcheck ./
