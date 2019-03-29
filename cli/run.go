d01dca9899326d294b906ffb5a91821017fbf299
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-03-29 16:58:40 +0100

    Lowercasing logrus import path

--- cli/run.go
+++ cli/run.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	log "github.com/Sirupsen/logrus"
 	"github.com/bitrise-io/envman/envman"
 	"github.com/bitrise-io/envman/models"
 	"github.com/bitrise-io/go-utils/command"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
