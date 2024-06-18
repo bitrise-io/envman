85858fa736220a545b0228c447624b9199fe9669
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-06-18 17:04:48 +0200

    Make convertToEnvsJSONModel exported

--- cli/print_test.go
+++ cli/print_test.go
@@ -18,14 +18,14 @@ envs:
 	environments, err := ParseEnvsYML([]byte(envsStr))
 	require.Equal(t, nil, err)
 
-	envsJSONList, err := convertToEnvsJSONModel(environments, false, false, &env.DefaultEnvironmentSource{})
+	envsJSONList, err := ConvertToEnvsJSONModel(environments, false, false, &env.DefaultEnvironmentSource{})
 	require.Equal(t, nil, err)
 	require.Equal(t, "$HOME", envsJSONList["TEST_HOME1"])
 	require.Equal(t, "$TEST_HOME1/test", envsJSONList["TEST_HOME2"])
 
 	testHome1 := os.Getenv("HOME")
 	testHome2 := path.Join(testHome1, "test")
-	envsJSONList, err = convertToEnvsJSONModel(environments, true, false, &env.DefaultEnvironmentSource{})
+	envsJSONList, err = ConvertToEnvsJSONModel(environments, true, false, &env.DefaultEnvironmentSource{})
 	require.Equal(t, nil, err)
 	require.Equal(t, testHome1, envsJSONList["TEST_HOME1"])
 	require.Equal(t, testHome2, envsJSONList["TEST_HOME2"])
@@ -43,13 +43,13 @@ envs:
 	require.Equal(t, nil, err)
 
 	// print everything
-	envsJSONList, err := convertToEnvsJSONModel(environments, false, false, &env.DefaultEnvironmentSource{})
+	envsJSONList, err := ConvertToEnvsJSONModel(environments, false, false, &env.DefaultEnvironmentSource{})
 	require.Equal(t, nil, err)
 	require.Equal(t, "testvalue", envsJSONList["nonsensitivekey"])
 	require.Equal(t, "testsecret", envsJSONList["sensitivekey"])
 
 	// print sensitive only
-	envsJSONList, err = convertToEnvsJSONModel(environments, true, true, &env.DefaultEnvironmentSource{})
+	envsJSONList, err = ConvertToEnvsJSONModel(environments, true, true, &env.DefaultEnvironmentSource{})
 	require.Equal(t, nil, err)
 	require.Equal(t, "", envsJSONList["nonsensitivekey"])
 	require.Equal(t, "testsecret", envsJSONList["sensitivekey"])
