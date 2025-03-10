5bae1f1d8f874a148bcaaad1d7b5be1e2a135457
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-03-10 16:16:13 +0100

    Fix integration test for add command

--- _tests/integration/helper.go
+++ _tests/integration/helper.go
@@ -1,7 +1,27 @@
 package integration
 
-import "os"
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var binPathStr string
 
 func binPath() string {
-	return os.Getenv("INTEGRATION_TEST_BINARY_PATH")
+	if binPathStr != "" {
+		return binPathStr
+	}
+
+	pth := os.Getenv("INTEGRATION_TEST_BINARY_PATH")
+	if pth == "" {
+		if os.Getenv("CI") == "true" {
+			panic("INTEGRATION_TEST_BINARY_PATH env is required in CI")
+		} else {
+			log.Warn("INTEGRATION_TEST_BINARY_PATH is not set, make sure 'bitrise' binary in your PATH is up-to-date")
+			pth = "envman"
+		}
+	}
+	binPathStr = pth
+	return pth
 }
