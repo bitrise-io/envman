4c09bc5356c4032ca4d22cfc970cec6882051101
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2026-06-09 22:31:00 +0200

    Env size limit (#248)
    * Revert "Allow overriding env byte limits via environment variables (#245)"
    
    This reverts commit 3b29173c13f381693c7688da9cc6097a59c2ec8d.
    
    * Bump default env var limits to 500 KB

--- envman/configs.go
+++ envman/configs.go
@@ -2,10 +2,8 @@ package envman
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path"
-	"strconv"
 
 	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/pathutil"
@@ -13,13 +11,8 @@ import (
 
 const (
 	envmanConfigFileName         = "configs.json"
-	defaultEnvBytesLimitInKB     = 256
-	defaultEnvListBytesLimitInKB = 256
-
-	// Environment variables to override the byte limits, take precedence over the config file.
-	// Useful when the config file is not writable/reachable, e.g. while running a script step.
-	envBytesLimitInKBEnvKey     = "ENVMAN_ENV_BYTES_LIMIT_IN_KB"
-	envListBytesLimitInKBEnvKey = "ENVMAN_ENV_LIST_BYTES_LIMIT_IN_KB"
+	defaultEnvBytesLimitInKB     = 500
+	defaultEnvListBytesLimitInKB = 500
 )
 
 // ConfigsModel ...
@@ -57,63 +50,37 @@ func createDefaultConfigsModel() ConfigsModel {
 // GetConfigs ...
 func GetConfigs() (ConfigsModel, error) {
 	configPth := getEnvmanConfigsFilePath()
-	configs := createDefaultConfigsModel()
+	defaultConfigs := createDefaultConfigsModel()
 
-	isExist, err := pathutil.IsPathExists(configPth)
+	if isExist, err := pathutil.IsPathExists(configPth); err != nil {
+		return ConfigsModel{}, err
+	} else if !isExist {
+		return defaultConfigs, nil
+	}
+
+	bytes, err := fileutil.ReadBytesFromFile(configPth)
 	if err != nil {
 		return ConfigsModel{}, err
 	}
 
-	if isExist {
-		bytes, err := fileutil.ReadBytesFromFile(configPth)
-		if err != nil {
-			return ConfigsModel{}, err
-		}
-
-		type ConfigsFileMode struct {
-			EnvBytesLimitInKB     *int `json:"env_bytes_limit_in_kb,omitempty"`
-			EnvListBytesLimitInKB *int `json:"env_list_bytes_limit_in_kb,omitempty"`
-		}
-
-		var userConfigs ConfigsFileMode
-		if err := json.Unmarshal(bytes, &userConfigs); err != nil {
-			return ConfigsModel{}, err
-		}
-
-		if userConfigs.EnvBytesLimitInKB != nil {
-			configs.EnvBytesLimitInKB = *userConfigs.EnvBytesLimitInKB
-		}
-		if userConfigs.EnvListBytesLimitInKB != nil {
-			configs.EnvListBytesLimitInKB = *userConfigs.EnvListBytesLimitInKB
-		}
+	type ConfigsFileMode struct {
+		EnvBytesLimitInKB     *int `json:"env_bytes_limit_in_kb,omitempty"`
+		EnvListBytesLimitInKB *int `json:"env_list_bytes_limit_in_kb,omitempty"`
 	}
 
-	// Environment variables take precedence over the config file, so the limits stay
-	// overrideable even when the config file is not reachable (e.g. inside a script step).
-	if err := applyEnvVarOverrides(&configs); err != nil {
+	var userConfigs ConfigsFileMode
+	if err := json.Unmarshal(bytes, &userConfigs); err != nil {
 		return ConfigsModel{}, err
 	}
 
-	return configs, nil
-}
+	if userConfigs.EnvBytesLimitInKB != nil {
+		defaultConfigs.EnvBytesLimitInKB = *userConfigs.EnvBytesLimitInKB
+	}
+	if userConfigs.EnvListBytesLimitInKB != nil {
+		defaultConfigs.EnvListBytesLimitInKB = *userConfigs.EnvListBytesLimitInKB
+	}
 
-// applyEnvVarOverrides overrides the limits from environment variables when set.
-func applyEnvVarOverrides(configs *ConfigsModel) error {
-	if val, ok := os.LookupEnv(envBytesLimitInKBEnvKey); ok {
-		limit, err := strconv.Atoi(val)
-		if err != nil {
-			return fmt.Errorf("invalid value (%s) for %s: %s", val, envBytesLimitInKBEnvKey, err)
-		}
-		configs.EnvBytesLimitInKB = limit
-	}
-	if val, ok := os.LookupEnv(envListBytesLimitInKBEnvKey); ok {
-		limit, err := strconv.Atoi(val)
-		if err != nil {
-			return fmt.Errorf("invalid value (%s) for %s: %s", val, envListBytesLimitInKBEnvKey, err)
-		}
-		configs.EnvListBytesLimitInKB = limit
-	}
-	return nil
+	return defaultConfigs, nil
 }
 
 // saveConfigs ...
